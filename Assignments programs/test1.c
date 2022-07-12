94ddee4ac1040e3c545b4e22f582dbf3084f180e
Tech-with-kundan <108984504+Tech-with-kundan@users.noreply.github.com>
2022-07-12 20:39:10 +0530

    Add files via upload

---
+++ Assignments programs/test1.c
@@ -0,0 +1,9 @@
+//1. Write a program to print unit digit of a given number
+#include<stdio.h>
+int main(void){
+     int n;
+     printf("Enter the number:");
+     scanf("%d", &n);
+     printf("The unit  number is %d", n%10);
+return 0;
+}
