13d2017934820d20666d180a66b7faf353934361
Tech-with-kundan <108984504+Tech-with-kundan@users.noreply.github.com>
2022-07-12 20:40:16 +0530

    Add files via upload

---
+++ Assignments programs/test6.c
@@ -0,0 +1,10 @@
+//6. Write a program which takes a character as an input and displays its ASCII code.
+#include<stdio.h>
+int main(void){
+char ch;
+printf("Enter the character:");
+scanf("%d", &ch);
+printf("The ASCII code of this character is %d", ch);
+
+return 0;
+}
