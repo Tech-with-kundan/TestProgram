34e0500badadecd4e379193a335ad9a7d888ac69
Tech-with-kundan <108984504+Tech-with-kundan@users.noreply.github.com>
2022-07-17 13:29:38 +0530

    Update test10.c

--- Test Assignment 5/test10.c
+++ Test Assignment 5/test10.c
@@ -1,8 +1,8 @@
-//10. Write a program to print a table of 5.
+//10. Write a program to print a table of N.
 #include<stdio.h>
 int main(void){
     int N;
-    printf("Enter the number as you  want  to print :");
+    printf("Enter the number as you want to print the table  :");
     scanf("%d", &N);
  for(int i=1; i<=10; i++)
       printf("%d\n", N*i);
