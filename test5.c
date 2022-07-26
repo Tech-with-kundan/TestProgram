7b259d9c1afb0970c95b078975d17efebca30cac
Tech-with-kundan <108984504+Tech-with-kundan@users.noreply.github.com>
2022-07-26 18:33:10 +0530

    Delete test5.c

--- test5.c
+++
@@ -1,7 +0,0 @@
-// program to  calculate the length of string using printf function .
-#include<stdio.h>
-int  main(void){
-     int x = printf("Welcome to the Programming world ");
-     printf("Length of string is %d ", x);
-return 0;
-}
