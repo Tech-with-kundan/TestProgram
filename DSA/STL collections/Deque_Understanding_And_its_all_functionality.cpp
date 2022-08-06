1c0dc227b3de6098934310ba546be5a0633ba49a
Kundan Kumar <108984504+Tech-with-kundan@users.noreply.github.com>
2022-08-06 15:47:09 +0530

    Update Deque_Understanding_And_its_all_functionality.cpp

--- DSA/STL collections/Deque_Understanding_And_its_all_functionality.cpp
+++ DSA/STL collections/Deque_Understanding_And_its_all_functionality.cpp
@@ -1,10 +1,13 @@
 /*
 Deque in C++ Standard Template Library (STL)
 
-Difficulty Level : Easy
-Last Updated : 06 Jul, 2022
-Double-ended queues are sequence containers with the feature of expansion and contraction on both ends. They are similar to vectors, but are more efficient in case of insertion and deletion of elements. Unlike vectors, contiguous storage allocation may not be guaranteed. 
-Double Ended Queues are basically an implementation of the data structure double-ended queue. A queue data structure allows insertion only at the end and deletion from the front. This is like a queue in real life, wherein people are removed from the front and added at the back. Double-ended queues are a special case of queues where insertion and deletion operations are possible at both the ends.
+
+Double-ended queues are sequence containers with the feature of expansion and contraction on both ends.
+They are similar to vectors, but are more efficient in case of insertion and deletion of elements. Unlike vectors, contiguous storage allocation may not be guaranteed. 
+Double Ended Queues are basically an implementation of the data structure double-ended queue. 
+A queue data structure allows insertion only at the end and deletion from the front. This is like a queue in real life,
+wherein people are removed from the front and added at the back.
+Double-ended queues are a special case of queues where insertion and deletion operations are possible at both the ends.
 The functions for deque are same as vector, with an addition of push and pop operations for both front and back.  
 
 The time complexities for doing various operations on deques are-
